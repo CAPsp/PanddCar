133e7abe2ffcfb21d0bace777e70da6b5fbc77f8
Masaya_Kobayashi <kobayashi03@hotmail.co.jp>
2016-10-30 04:13:13 +0900

    [add]gameシーン、みんなの成果物を反映

--- PandDCar/Assets/Scripts/CarHitAction.cs
+++ PandDCar/Assets/Scripts/CarHitAction.cs
@@ -1,27 +1,1 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class CarHitAction : MonoBehaviour {
-
-    AudioSource audioHit_;
-
-    void Awake() {
-        audioHit_ = GetComponent<AudioSource>();
-    }
-
-    void OnCollisionEnter(Collision other) {
-
-        // 敵ならSEを流す
-        if(other.gameObject.tag == "Enemy") {
-
-            if (audioHit_.isPlaying) {
-                audioHit_.Stop();
-            }
-
-            audioHit_.Play();
-
-        }
-
-    }
-
-}
+﻿using UnityEngine;using System.Collections;public class CarHitAction : MonoBehaviour {    AudioSource audioHit_;    void Awake() {        audioHit_ = GetComponent<AudioSource>();    }    void OnCollisionEnter(Collision other) {        // 敵ならSEを流す        if(other.gameObject.tag == "Enemy") {            if (audioHit_.isPlaying) {                audioHit_.Stop();            }            audioHit_.Play();        }    }}

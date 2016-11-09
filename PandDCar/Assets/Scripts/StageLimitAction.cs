c98be427e84beb811aaab7deed2a4f828bea90d9
Masaya_Kobayashi <kobayashi03@hotmail.co.jp>
2016-11-09 14:48:10 +0900

    [fix]commitできないバグを修正

---
+++ PandDCar/Assets/Scripts/StageLimitAction.cs
@@ -0,0 +1,3 @@
+﻿using UnityEngine;using System.Collections;public class StageLimitAction : MonoBehaviour {    [SerializeField] TimerControl timerControl_;    Vector3 carRespwanPoint_;    void Start() {
+        carRespwanPoint_ = GameObject.FindGameObjectWithTag("Player").transform.position;
+    }    void OnTriggerEnter(Collider other) {        // 車なら        if(other.gameObject.tag == "Player") {            timerControl_.Drop();            other.gameObject.transform.position = carRespwanPoint_ + new Vector3(0f, 5f, 0f);            Fader.instance.BlackIn();        }        // 敵なら        else if(other.gameObject.tag == "Enemy"){            Destroy(other.gameObject);        }            }}
